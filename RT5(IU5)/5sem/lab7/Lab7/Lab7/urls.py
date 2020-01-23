259ce20831a6b1882ee60eca5bddb548e5545da7
Makarov Alexey <4444666+Rapdigol@users.noreply.github.com>
2020-01-23 19:42:25 +0300

    Create urls.py

---
+++ RT5(IU5)/5sem/lab7/Lab7/Lab7/urls.py
@@ -0,0 +1,28 @@
+"""Lab7 URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django import views
+from django.conf.urls import url
+from django.contrib import admin
+from myapp.views import *
+
+
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^register/', RegisterFormView.as_view()),
+    url(r'^login/', LoginFormView.as_view()),
+    url(r'^users/$', UserList.as_view(), name='my-view2'),
+    url(r'^usersList/$', UserList.as_view()),
+]
