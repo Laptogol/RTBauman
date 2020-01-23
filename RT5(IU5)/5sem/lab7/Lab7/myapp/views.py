6179958b11eb70b3b5947f5ebe549458a8784945
Makarov Alexey <4444666+Rapdigol@users.noreply.github.com>
2020-01-23 19:43:10 +0300

    Create views.py

---
+++ RT5(IU5)/5sem/lab7/Lab7/myapp/views.py
@@ -0,0 +1,75 @@
+from django.http import HttpResponse
+from django.views.generic.edit import FormView
+from django.views.generic import ListView
+from django.contrib.auth.forms import UserCreationForm
+# Опять же, спасибо django за готовую форму аутентификации.
+from django.contrib.auth.forms import AuthenticationForm
+
+# Функция для установки сессионного ключа.
+# По нему django будет определять, выполнил ли вход пользователь.
+from django.contrib.auth import login
+from django.http import HttpResponseRedirect
+from django.views.generic.base import View
+from django.contrib.auth import logout
+
+
+from myapp.Connection import *
+
+
+class UserList(ListView):
+    model = User
+
+#####
+class UserList(View):
+    def get(self, request, *args, **kwargs):
+        con = Connection("root", "1111", "userbronkoncert")
+        with con:
+            user = SelAll(con)
+            f=user.save()
+        return HttpResponse(f)
+
+#######
+
+class LogoutView(View):
+    def get(self, request):
+        # Выполняем выход для пользователя, запросившего данное представление.
+        logout(request)
+
+        # После чего, перенаправляем пользователя на главную страницу.
+        return HttpResponseRedirect("/")
+
+
+class LoginFormView(FormView):
+    form_class = AuthenticationForm
+
+    # Аналогично регистрации, только используем шаблон аутентификации.
+    template_name = "login.html"
+
+    # В случае успеха перенаправим на главную.
+    success_url = "/users/"
+
+    def form_valid(self, form):
+        # Получаем объект пользователя на основе введённых в форму данных.
+        self.user = form.get_user()
+
+        # Выполняем аутентификацию пользователя.
+        login(self.request, self.user)
+        return super(LoginFormView, self).form_valid(form)
+
+
+class RegisterFormView(FormView):
+    form_class = UserCreationForm
+
+    # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
+    # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
+    success_url = "/login/"
+
+    # Шаблон, который будет использоваться при отображении представления.
+    template_name = "register.html"
+
+    def form_valid(self, form):
+        # Создаём пользователя, если данные в форму были введены корректно.
+        form.save()
+
+        # Вызываем метод базового класса
+        return super(RegisterFormView, self).form_valid(form)
